a82550c02092c05cf1f2dbb5180c0895e2bd46a3
abrodyyy <alexandra.brody@gmail.com>
2023-02-03 01:01:36 -0800

    Deliverable 2 complete

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,22 +1,53 @@
-# Deliverable 1: Linear Regression to Predict MPG
+ # File name of output log
+my_log <- file("hotel_bookings_statistical_analysis.txt")
+# Writing console output to log file
+sink(my_log, append = TRUE, type = "output")
+sink(my_log, append = TRUE, type = "message")
+# Writing currently opened R script to file
+cat(readChar(rstudioapi::getSourceEditorContext()$path,
+             file.info(rstudioapi::getSourceEditorContext()$path)$size))
 
-# 1.1 Download the MechaCar_mpg.csv file, and place it
-# in the active directory for your R session.
+## Deliverable 1: Linear Regression to Predict MPG
 
-# 1.2 Create a new RScript in your R source pane, name it
-# MechaCarChallenge.RScript, and save it to your active directory.
-
-# 1.3 Use the library() function to load the dplyr package.
+# Use the library() function to load the dplyr package.
 library(dplyr)
 
-# 1.4 Import and read in the MechaCar_mpg.csv file as a dataframe
-MechaCar <- read.csv("Resources/MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
-MechaCar
-# 1.5 Perform linear regression using the lm() function. 
-# In the lm() function, pass in all six variables (i.e., columns), 
-# and add the dataframe you created in Step 4 as the data parameter.
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
+# Import and read in the MechaCar_mpg.csv file as a dataframe
+mecha_car_df <- read.csv("Resources/MechaCar_mpg.csv",
+check.names = FALSE, stringsAsFactors = FALSE)
 
-# 1.6 Using the summary() function, determine the p-value and the r-squared 
+# Perform linear regression using the lm() function.
+# In the lm() function, pass in all six variables and add the dataframe.
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle
+ + ground_clearance + AWD, data = mecha_car_df)
+
+# Using the summary() function, determine the p-value and the r-squared
 # value for the linear regression model.
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle
++ ground_clearance + AWD, data = mecha_car_df))
+
+## Part 2: Create Visualizations for the Trip Analysis
+
+# Import and read in the Suspension_Coil.csv file as a table.
+suspension_coil_table <- read.csv("Resources/Suspension_Coil.csv",
+check.names = FALSE, stringsAsFactors = FALSE)
+
+# Create a total_summary dataframe.
+# Get summary statistics for the suspension coil’s PSI column.
+total_summary_df <- suspension_coil_table %>%
+  summarize(Mean = mean(PSI),
+            Medain = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI), .groups = "KEEP")
+
+# Create a lot_summary dataframe.
+# Get summary statistics for the suspension coil’s PSI column.
+lot_summary_df <- suspension_coil_table %>%
+  group_by(manufacturing_lot) %>%
+  summarize(Mean = mean(PSI),
+            Medain = median(PSI)
+            Variance = var(PSI),
+            SD = sd(PSI), .groups = "KEEP")
+
+# Close connection to log file
+closeAllConnections()
--- README.md
+++ README.md
@@ -22,3 +22,8 @@ This new assignment consists of three technical analysis parts and a proposal fo
 ## Resources
 - Data Source: [MechaCar_mpg.csv](link), [Suspension_Coil.csv](link)
 - Software: [R Studio](link)
+
+## Deliverable 1: Linear Regression to Predict MPG
+- Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+- Is the slope of the linear model considered to be zero? Why or why not?
+- Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
